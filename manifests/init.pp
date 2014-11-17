99ce5e7db55b35602f3d0f53ee0eea77ff914122
Michael Ferguson <mpherg@gmail.com>
2014-11-17 15:00:42 -0700

    Install for all ruby versions

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,8 @@
 # Installs cocoapods
 #     Use: "include cocoapods"
 class cocoapods {
-  require ruby::global
-
-  ruby_gem { 'cocoapods':
-    gem  => 'cocoapods',
-    ruby_version => $::ruby::global::version,
+  ruby_gem { "cocoapods":
+    gem  => "cocoapods",
+    ruby_version => "*",
   }
 }
